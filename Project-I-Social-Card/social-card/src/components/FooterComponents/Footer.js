81cec40790432e2c031b07b9cc51c2f12ff55884
Julian Moreno <julmo611@gmail.com>
2019-02-11 16:08:54 -0800

    MVP Done

---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Footer.css';
+
+
+const Footer = () => {
+    return (
+        <div className="footer-content">
+            <div><img src="images/comment.png" alt="Comments"/></div>
+            <div><img src="images/recycle.png" alt="Arrows"/> 6</div>
+            <div><img src="images/heart.png" alt="Heart"/> 4</div>
+            <div><img src="images/mail.png" alt="Email"/></div>            
+        </div>
+    );
+}
+
+export default Footer;
+
