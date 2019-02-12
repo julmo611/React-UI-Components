dcf0fdaa243130d9c3c0b1013758a1c637c667c5
Julian Moreno <julmo611@gmail.com>
2019-02-12 12:27:46 -0800

    Starting point project II

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import moment from 'moment';
 import './Header.css';
 
 
 const HeaderTitle = () => {
     return (
        <div className="header-content">
-           <p><strong>Lambda School</strong> <a href="https://twitter.com/lambdaschool" target="_blank">@LambdaSchool 26 jan</a></p>
+           <p><strong>Lambda School</strong> <a href="https://twitter.com/lambdaschool" target="_blank">@LambdaSchool {moment().format('DD MMM')}</a></p>
            <p>Let's learn React by bulding simple interdaces with components. Don't try to overthink it, just keep it simple and have fun. Once you feel comfortable using components you are well on your way to mastering React!</p>
        </div>
     );
